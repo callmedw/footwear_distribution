d99231d6c082e452ae80066f31950fdcb010a677
Dana Weiss <mynameisdanaweiss@gmail.com>
2017-05-12 13:39:00 -0700

    add validations for presence and maximum length for name of the Store class

--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -2,4 +2,10 @@ require "spec_helper"
 
 describe(Store) do
   it { should have_and_belong_to_many(:brands) }
+  it { should validate_presence_of(:name) }
+
+  it("returns false if it's too long store name") do
+    store = Store.create({name: "'It is great to be known for your shoes but it is better to be known for your sole' says Kenneth Cole, but on the other hand Cinderalla proved that a new pair of shoes can change your life"})
+    expect(store.save).to eq(false)
+  end
 end
