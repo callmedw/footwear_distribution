0a499cacab79f10f91212555d37dbe7a6676ae97
Dana Weiss <mynameisdanaweiss@gmail.com>
2017-05-12 09:32:11 -0700

    add basic pages for brands and stores

--- app.rb
+++ app.rb
@@ -5,7 +5,17 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 DATABASE_URL="postgres://localhost/footwear_development"
 
 get '/' do
+  erb :index
+end
+
+get '/brands' do
+  @brands=Brand.all
+  @stores = Store.all
+  erb :brands
+end
+
+get '/stores' do
   @stores=Store.all
   @brands = Brand.all
-  erb :index
+  erb :stores
 end
