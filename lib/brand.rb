61569a19af2e2a1cb61f76fe0e4226fe7bd028e5
Dana Weiss <mynameisdanaweiss@gmail.com>
2017-05-12 13:37:07 -0700

    add validations for presence of name and cost and maximum length for name of the Brand class

--- lib/brand.rb
+++ lib/brand.rb
@@ -1,3 +1,6 @@
 class Brand < ActiveRecord::Base
   has_and_belongs_to_many(:stores)
+  validates(:name, {:length => { :maximum => 100 }})
+  validates(:name, presence: true)
+  validates(:cost, presence: true)
 end
--- spec/brand_spec.rb
+++ spec/brand_spec.rb
@@ -2,4 +2,11 @@ require "spec_helper"
 
 describe(Brand) do
   it { should have_and_belong_to_many(:stores) }
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:cost) }
+
+  it("returns false if it's too long brand name") do
+    brand = Brand.create({name: "'It is great to be known for your shoes but it is better to be known for your sole' says Kenneth Cole, but on the other hand Cinderalla proved that a new pair of shoes can change your life"})
+    expect(brand.save).to eq(false)
+  end
 end
