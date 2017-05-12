87adf1442ee323efa7b0ad69f725e6dbca86c1e6
Dana Weiss <mynameisdanaweiss@gmail.com>
2017-05-12 14:42:01 -0700

    add passing spec for add a new brand pathway in footwear_integration_spec

--- spec/footwear_integration_spec.rb
+++ spec/footwear_integration_spec.rb
@@ -6,3 +6,17 @@ describe 'the index landing page', {:type => :feature} do
     expect(page).to have_content "Footwear Distribution Center"
   end
 end
+
+describe 'the add a new brand pathway', {:type => :feature} do
+  it 'ENTER STUFF HERE' do
+    visit '/'
+    expect(page).to have_content "Footwear Distribution Center"
+    click_link('brands')
+    expect(page).to have_content "Add a new brand to distribution"
+    fill_in('name', :with => 'Cool Shoe Brand')
+    fill_in('cost', :with => 500.00)
+    click_button('Add Brand')
+    expect(page).to have_content "Cool Shoe Brand"
+
+  end
+end
