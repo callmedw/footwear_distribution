e814c087ddeccb827f13b68c2ec04ed4cf86d0a3
Dana Weiss <mynameisdanaweiss@gmail.com>
2017-05-12 14:28:54 -0700

    add passing spec for index landing page in footwear_integration_spec

--- spec/footwear_integration_spec.rb
+++ spec/footwear_integration_spec.rb
@@ -1,1 +1,8 @@
 require "spec_helper"
+
+describe 'the index landing page', {:type => :feature} do
+  it 'shows title and introduces site navigation' do
+    visit '/'
+    expect(page).to have_content "Footwear Distribution Center"
+  end
+end
