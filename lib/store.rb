25b08f2e8744b644a5da57682cdfa254e8d83996
Dana Weiss <mynameisdanaweiss@gmail.com>
2017-05-12 14:07:30 -0700

    add name uniqueness validation to class Store

--- lib/store.rb
+++ lib/store.rb
@@ -2,4 +2,5 @@ class Store < ActiveRecord::Base
   has_and_belongs_to_many(:brands)
   validates(:name, {:length => { :maximum => 100 }})
   validates(:name, presence: true)
+  validates :name, uniqueness: { case_sensitive: false }
 end
