af5c1de7e466af7f22b1dc88b4a2a8c83d8cb2f6
Dana Weiss <mynameisdanaweiss@gmail.com>
2017-05-12 10:00:58 -0700

    add create brand to brand landing page and update and delete functionality to brand/id page

--- app.rb
+++ app.rb
@@ -8,14 +8,36 @@ get '/' do
   erb :index
 end
 
+# brand pages
 get '/brands' do
-  @brands=Brand.all
-  @stores = Store.all
+  @brands = Brand.all
   erb :brands
 end
 
+post '/brands' do
+  Brand.create(:name => params['name'])
+  redirect '/brands'
+end
+
+get '/brand/:id' do
+  @brand = Brand.find(params['id'].to_i)
+  erb :brand
+end
+
+patch '/brand/:id' do
+  @brand = Brand.find(params['id'].to_i)
+  @brand.update(:name => params['name'])
+  redirect '/brands'
+end
+
+delete '/brand/:id' do
+  brand = Brand.find(params['id'].to_i)
+  brand.delete
+  redirect '/brands'
+end
+
+# store pages
 get '/stores' do
   @stores=Store.all
-  @brands = Brand.all
   erb :stores
 end
